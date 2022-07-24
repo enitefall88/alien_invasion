1dd142a4ec92df4b9186cd37865c8f4e4f27d422
Anton Durandin <enitefall81@gmail.com>
2022-07-24 12:47:11 -0700

    Left and right movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,17 +25,33 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
+            self._check_events()
+            self._update_screen()
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+
 
             # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+
+    def _check_events(self):
+        """Helper method stars with an underscore"""
+        #Respond to keypresses and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                # Move the ship to the right.
+                    self.ship.rect.x += 10
+                if event.key == pygame.K_LEFT:
+                    self.ship.rect.x -= 10
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
 
 if __name__ == '__main__':
--- ship.py
+++ ship.py
@@ -4,6 +4,7 @@ import pygame
 class Ship:
     "A class to manage the ship."""
     def __init__(self, ai_game):
+        #ai_game is the reference to the AlienInvasion object
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
