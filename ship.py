86ebec3ae7d548b93b0bf5cb8df275c07db0b728
Anton Durandin <enitefall81@gmail.com>
2022-07-26 09:49:38 -0700

    Set left and right boundaries for the ship movements

--- ship.py
+++ ship.py
@@ -24,10 +24,10 @@ class Ship:
         self.moving_left = False
 
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
 
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         self.rect.x = self.x
