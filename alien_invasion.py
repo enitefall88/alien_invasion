76aaf872272115c35c08e45d5c0f412ce7f92013
Anton Durandin <enitefall81@gmail.com>
2022-08-14 10:19:57 -0700

    sds

--- alien_invasion.py
+++ alien_invasion.py
@@ -67,7 +67,7 @@ class AlienInvasion:
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-        #todo
+
 
 
 
