b71ac4ae3b25670e621fde7dea64561aeb8bcaf7
Anton Durandin <enitefall81@gmail.com>
2022-08-14 11:21:02 -0700

    Moving update bullets into a helper method

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,10 +27,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
+            self._update_bullets()
             self._update_screen()
             # Watch for keyboard and mouse events.
 
@@ -82,8 +79,14 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
 
-
-
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
