b09f90fec7bf23f286ce4c01dd104351a7ca3b3d
Anton Durandin <enitefall81@gmail.com>
2022-08-14 11:16:33 -0700

    Limiting number of bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,7 +31,6 @@ class AlienInvasion:
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
-            print(len(self.bullets))
             self._update_screen()
             # Watch for keyboard and mouse events.
 
@@ -65,8 +64,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _check_keyup_events(self, event):
         """Respond to key releases."""
