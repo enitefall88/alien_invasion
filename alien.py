ab5468ad1ad61996cc5707c139ff0728d588b85a
enitefall <enitefall81@gmail.com>
2024-05-27 15:12:31 -0700

    added Button

--- alien.py
+++ alien.py
@@ -3,10 +3,13 @@ from pygame.sprite import Sprite
 
 
 class Alien(Sprite):
+    """A class to represent a single alien in the fleet."""
+
     def __init__(self, ai_game):
         """Initialize the alien and set its starting position."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load the alien image and set its rect attribute.
         self.image = pygame.image.load('images/alien.bmp')
@@ -18,3 +21,15 @@ class Alien(Sprite):
 
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
+
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """Move the alien right or left."""
+        self.x += (self.settings.alien_speed *
+                   self.settings.fleet_direction)
+        self.rect.x = self.x
--- bullet.py
+++ bullet.py
@@ -1,8 +1,10 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Bullet(Sprite):
     """A class to manage bullets fired from the ship"""
+
     def __init__(self, ai_game):
         """Create a bullet object at the ship's current position."""
         super().__init__()
